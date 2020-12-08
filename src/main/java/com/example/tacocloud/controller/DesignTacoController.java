ddbcbc2384c06c6ab35e08e36307b872125e5dc4
Хамидулла Казимов <C:\Users\kazimov\AppData\Roaming\The Bat!>
2020-12-08 17:38:11 +0700

    Added form validation

--- src/main/java/com/example/tacocloud/controller/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controller/DesignTacoController.java
@@ -6,10 +6,13 @@ import com.example.tacocloud.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,6 +24,27 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
+        setIngredientsToModel(model);
+
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design,
+                                Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            setIngredientsToModel(model);
+            model.addAttribute("design", design);
+            return "design";
+        }
+
+        log.info("Processing design " + design);
+
+        return "redirect:/orders/current";
+    }
+
+    private void setIngredientsToModel(Model model) {
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", IngredientType.WRAP),
                 new Ingredient("COTO", "Corn Tortilla", IngredientType.WRAP),
@@ -38,17 +62,7 @@ public class DesignTacoController {
         for (IngredientType type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     ingredients.stream().filter(x -> x.getType().equals(type))
-                                        .collect(Collectors.toList()));
+                            .collect(Collectors.toList()));
         }
-
-        model.addAttribute("design", new Taco());
-        return "design";
-    }
-
-    @PostMapping
-    public String processDesign(Taco design) {
-        log.info("Processing design " + design);
-
-        return "redirect:/orders/current";
     }
 }
--- src/main/java/com/example/tacocloud/model/Order.java
+++ src/main/java/com/example/tacocloud/model/Order.java
@@ -1,17 +1,30 @@
 package com.example.tacocloud.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class Order {
 
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Street is required")
     private String street;
+    @NotBlank(message = "City is required")
     private String city;
+    @NotBlank(message = "State is required")
     private String state;
+    @NotBlank(message = "Zip is required")
     private String zip;
+
+    @CreditCardNumber(message = "Invalid credit card number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
+    @Pattern(regexp = "\\d{3}", message = "Invalid CVV")
     private String ccCVV;
 
 }
